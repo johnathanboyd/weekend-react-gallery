4dae56becf033c54ccc41c140de099db3f9c3007
Johnathan <johnathanboyd5@gmail.com>
2021-03-28 08:02:36 -0500

    displayed images properly to start adding toggles and click handlers!

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,10 +1,23 @@
 import './GalleryItem.css';
+import { useState } from 'react'
 
 function GalleryItem(props){
+
+    const[showImage, setShowImage ] = useState( false );
+
+    let displayImage = () =>{
+        let display = <img className="image" src={props.image.path}></img>
+        if ( showImage) {
+            display = <div className="description" onClick = {tocggleContent}><p>{ props.image.description}</p></div>
+        }
+        return display
+    }
+
+    let toggleContent
     return(
         <>
-          <div className="image">
-            <img src={props.image.path}></img>
+          <div className="post">
+            {displayImage()}  
             <button>Like</button>
             <p> people have liked this.</p>
           </div>
